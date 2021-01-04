963d53b8a11df057cccff087250d64078c6ed585
ZipS <czwetkow.serega@yandex.ru>
2021-01-04 10:27:11 +0300

    game settings and developer cfg are now different files

--- constants.py
+++
@@ -1,9 +0,0 @@
-# colors
-WHITE = (240,240,240)
-BLACK = (0,0,0)
-RED = (240,0,0)
-
-# game constants
-VOID = 0
-ZERO = 1
-CROSS = 2
---
+++ settings.py
@@ -0,0 +1,4 @@
+#settings
+ANTI_ALIAS = True
+TICKRATE = 60
+EGG_ENABLED = False
